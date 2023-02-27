4e4e87e1556fa5122af6ac5440539efc821295a3
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-02-27 13:39:58 -0600

    change title

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Well, hello there.
+          Well, hello world.
         </p>
         <a
           className="App-link"
