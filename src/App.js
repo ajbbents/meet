f520de8fa55d9c8f5f2fe84011422d88b883786f
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-02-28 11:38:41 -0600

    construct and test citysearch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import EventList from './EventList';
+import CitySearch from './CitySearch';
 
 import './App.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <CitySearch />
         <EventList />
       </div>
     );
