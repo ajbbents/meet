2d7078407eb7f2092608e0ae2d97271d841da1c4
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-02-28 11:00:41 -0600

    add and test eventlist function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import EventList from './EventList';
 
 import './App.css';
 
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <EventList />
       </div>
     );
   }
