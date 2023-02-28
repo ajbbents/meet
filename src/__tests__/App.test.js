74ae7188f8916b83079506ce09ad0900dc7b0675
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-02-28 11:41:14 -0600

    apply beforeall

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -5,13 +5,15 @@ import EventList from '../EventList';
 import CitySearch from '../CitySearch';
 
 describe('<App /> component', () => {
-    test('render list of events', () => {
-        const AppWrapper = shallow(<App />);
-        expect(AppWrapper.find(EventList)).toHaveLength(1);
-    });
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
+  test('render list of events', () => {
+    expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
 
-    test('render CitySearch', () => {
-        const AppWrapper = shallow(<App />);
-        expect(AppWrapper.find(CitySearch)).toHaveLength(1);
-    });
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
 });
