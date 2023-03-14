db7fef0378f8ad45991e9765a73dfa6fbe971734
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-03-14 10:30:42 -0500

    create and execute gherkin tests for scen 2, 3

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,8 +1,10 @@
 import { loadFeature, defineFeature } from "jest-cucumber";
 import React from "react";
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import App from "../App";
 import { mockData } from '../mock-data';
+import CitySearch from "../CitySearch";
+import { extractLocations } from "../api";
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 defineFeature(feature, test => {
@@ -25,40 +27,47 @@ defineFeature(feature, test => {
   // Feature file has a scenario titled "User should see a list of suggestions when they search for a city", but no match found in step definitions. Try adding the following code:
 
   test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
-    given('the main page is open', () => {
 
+    let CitySearchWrapper;
+    const locations = extractLocations(mockData);
+    given('the main page is open', () => {
+      CitySearchWrapper = shallow(<CitySearch updateEvents={() => { }} locations={locations} />);
     });
 
     when('the user starts typing in the city textbox', () => {
-
+      CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
     });
 
     then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
-
+      expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(2);
     });
   });
 
   // Feature file has a scenario titled "User can select a city from the suggested list", but no match found in step definitions. Try adding the following code:
 
   test('User can select a city from the suggested list', ({ given, and, when, then }) => {
-    given('user was typing “Berlin” in the city textbox', () => {
-
+    let AppWrapper;
+    given('user was typing “Berlin” in the city textbox', async () => {
+      AppWrapper = await mount(<App />);
+      AppWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
     });
 
     and('the list of suggested cities is showing', () => {
-
+      AppWrapper.update();
+      expect(AppWrapper.find('.suggestions li')).toHaveLength(2);
     });
 
     when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
-
+      AppWrapper.find('.suggestions li').at(0).simulate('click');
     });
 
     then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
-
+      const CitySearchWrapper = AppWrapper.find(CitySearch);
+      expect(CitySearchWrapper.state('query')).toBe('Berlin, Germany');
     });
 
     and('the user should receive a list of upcoming events in that city', () => {
-
+      expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
     });
   });
 });
