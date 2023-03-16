b682c75d66e3829fa4d0f6ed4f0ea027b6f58b99
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-03-16 11:45:31 -0500

    initialize and construct tests for number ofevents

--- src/features/specifyNumberOfEvents.feature
+++ src/features/specifyNumberOfEvents.feature
@@ -1,9 +1,9 @@
 Feature: Specify number of events
 
-Scenario: When a user hasn't specified a number, 10 is the default number
-Given the user hasn't indicated how many events to view
+Scenario: When a user has not specified a number, ten is the default number
+Given the user has not indicated how many events to view
 When user opens the app
-Then user should see 10 events by default
+Then user should see ten events by default
 
 Scenario: User can change number of events they want to see
 Given user hasn't entered number of events to user input
--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -7,31 +7,36 @@ const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
 defineFeature(feature, test => {
   let AppWrapper;
-  test('When a user hasn\'t specified a number, 10 is the default number', ({ given, when, then }) => {
-    given('the user hasn\'t indicated how many events to view', () => {
+  test('When a user has not specified a number, ten is the default number', ({ given, when, then }) => {
+    given('the user has not indicated how many events to view', () => {
 
     });
 
     when('user opens the app', () => {
-
+      AppWrapper = mount(<App />);
     });
 
-    then(/^user should see (\d+) events by default$/, (arg0) => {
-
+    then('user should see ten events by default', () => {
+      AppWrapper.update();
+      expect(AppWrapper.state('numberOfEvents')).toEqual(10);
     });
   });
 
   test('User can change number of events they want to see', ({ given, when, then }) => {
     given('user hasn\'t entered number of events to user input', () => {
-
+      AppWrapper = mount(<App />);
     });
 
     when('user indicates number of events to user input', () => {
-
+      AppWrapper.update();
+      const NumberOfEventsWrapper = AppWrapper.find('NumberOfEvents');
+      const eventObject = { target: { value: 7 } };
+      NumberOfEventsWrapper.find('.num').simulate('change', eventObject);
     });
 
     then('number of displayed events should equal indicated number', () => {
-
+      AppWrapper.update();
+      expect(AppWrapper.state('numberOfEvents')).toEqual(7);
     });
   });
 });
