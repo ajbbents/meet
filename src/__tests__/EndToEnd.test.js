92d53b22c329745f1cdd747a4336e2f440e2381f
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-03-16 12:40:37 -0500

    create and run endtoend test for second feature

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,37 +1,85 @@
 import puppeteer from "puppeteer";
 
-describe('show/hide an event details', () => {
+describe('filter events by city', () => {
   let browser;
   let page;
+  jest.setTimeout(100000);
   beforeAll(async () => {
     browser = await puppeteer.launch({
       headless: false,
-      slowMo: 250, // slow down by 250 ms
-      ignoreDefaultArgs: ['--disable-extensions'] //ignores default setting that causes timeout errors
+      slowMo: 250,
+      ignoreDefaultArgs: ['--disable-extensions']
     });
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.event');
-  }, 30000); //30000 is the timeout
+  });
 
   afterAll(() => {
     browser.close();
   });
 
-  test('An event element is collapsed by default', async () => {
-    const eventDetails = await page.$('.event .event-details');
-    expect(eventDetails).toBeNull();
+  test('when user has not searched for a city show events from all cities', async () => {
+    const numberOfEvents = await page.$$eval(
+      '.event',
+      (element) => element.length
+    );
+    expect(numberOfEvents).toBe(2);
   });
 
-  test('User can expand an event to see its details', async () => {
-    await page.click('.event .details-button');
-    const eventDetails = await page.$('.event .event-details');
-    expect(eventDetails).toBeDefined();
+  test('user should see a list of suggestions when they search for a city', async () => {
+    await page.type('.city', 'Berlin', { delay: 5 });
+    const numberOfCities = await page.$$eval(
+      '.suggestions li',
+      (element) => element.length
+    );
+    expect(numberOfCities).toBe(2);
   });
 
-  test('User can collapse event to hide its details', async () => {
-    await page.click('.event .details-button');
-    const eventDetails = await page.$('.event .event-details');
-    expect(eventDetails).toBeNull();
+  test('user can select a city from list of suggestions', async () => {
+    await page.reload();
+    await page.type('.city', 'Berlin', { delay: 5 });
+    await page.click('.suggestions li');
+    const numberOfEvents = await page.$$eval(
+      '.event',
+      (element) => element.length
+    );
+    expect(numberOfEvents).toBe(1);
   });
 });
+
+// describe('show/hide an event details', () => {
+//   let browser;
+//   let page;
+//   beforeAll(async () => {
+//     browser = await puppeteer.launch({
+//       headless: true,
+//       slowMo: 250, // slow down by 250 ms
+//       ignoreDefaultArgs: ['--disable-extensions'] //ignores default setting that causes timeout errors
+//     });
+//     page = await browser.newPage();
+//     await page.goto('http://localhost:3000/');
+//     await page.waitForSelector('.event');
+//   }, 30000); //30000 is the timeout
+
+//   afterAll(() => {
+//     browser.close();
+//   });
+
+//   test('An event element is collapsed by default', async () => {
+//     const eventDetails = await page.$('.event .event-details');
+//     expect(eventDetails).toBeNull();
+//   });
+
+//   test('User can expand an event to see its details', async () => {
+//     await page.click('.event .details-button');
+//     const eventDetails = await page.$('.event .event-details');
+//     expect(eventDetails).toBeDefined();
+//   });
+
+//   test('User can collapse event to hide its details', async () => {
+//     await page.click('.event .details-button');
+//     const eventDetails = await page.$('.event .event-details');
+//     expect(eventDetails).toBeNull();
+//   });
+// });
