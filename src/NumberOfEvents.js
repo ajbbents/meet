d5f3fa9b8e47a162807f17248a14e5eb06517d6f
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-03-22 15:49:08 -0500

    update number of events

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -4,20 +4,20 @@ import { ErrorAlert } from './Alert';
 class NumberOfEvents extends Component {
   state = {
     query: '',
-    num: 32,
+    num: 10,
   };
 
   changeNum = (value) => {
     this.setState({ num: value });
     this.props.updateNumberOfEvents(value);
-    if (value < 1 || value > 32) {
+    if (value < 1 || value > 10) {
       this.setState({ errorText: 'Please select a value between 1 and 10' });
     } else this.setState({ errorText: '' });
     console.log(this.props);
   };
 
   componentDidMount() {
-    this.setState({ num: this.props.num || 32 });
+    this.setState({ num: this.props.num || 10 });
   }
 
   render() {
