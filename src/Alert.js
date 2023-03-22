3161a4beffce894555811eee15b0330a3964ffa6
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-03-22 15:42:01 -0500

    put warningalert on hold

--- src/Alert.js
+++ src/Alert.js
@@ -28,12 +28,12 @@ class InfoAlert extends Alert {
   }
 }
 
-class WarningAlert extends Alert {
-  constructor(props) {
-    super(props);
-    this.color = 'orange';
-  }
-}
+// class WarningAlert extends Alert {
+//   constructor(props) {
+//     super(props);
+//     this.color = 'orange';
+//   }
+// }
 
 class ErrorAlert extends Alert {
   constructor(props) {
