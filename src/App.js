ea352a846777060ed56bfa034170a1894c1c6f62
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-03-29 14:56:34 -0500

    add responsiveness to chart

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { extractLocations, getEvents } from './api';
 import './nprogress.css';
 import './App.css';
 import { WarningAlert } from './Alert';
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 class App extends Component {
   state = {
@@ -82,19 +82,15 @@ class App extends Component {
             this.updateNumberOfEvents(num)}
         />
         <h4>Events in each city:</h4>
-        <ScatterChart
-          width={800}
-          height={400}
-          margin={{
-            top: 20, right: 20, bottom: 20, left: 20,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="city" type="category" name="city" />
-          <YAxis dataKey="number" type="number" name="number of events" allowDecimals={false} />
-          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-          <Scatter data={this.getData()} fill="8884d8" />
-        </ScatterChart>
+        <ResponsiveContainer height={400} >
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+            <CartesianGrid />
+            <XAxis dataKey="city" type="category" name="city" />
+            <YAxis dataKey="number" type="number" name="number of events" allowDecimals={false} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList events={this.state.events} />
 
       </div>
