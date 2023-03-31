2be3a92d6d6a4fc469debd736845ff21ef660200
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-03-31 10:49:58 -0500

    Add pie chart

---
+++ src/EventGenre.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from 'recharts';
+
+const EventGenre = ({ events }) => {
+  const [data, setData] = useState({});
+  useEffect(() => { setData(() => getData()); }, [events]);
+
+  const getData = () => {
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+    // const summaries = events.map(event => event.summary);
+    const data = genres.map((genre) => {
+      const value = events.filter(({ summary }) =>
+        summary.split(' ').includes(genre)
+      ).length;
+      return { name: genre, value };
+    });
+    return data;
+  };
+
+  const colors = ['#3A5261', '#4A3A61', '#B06101', '#613A52', '#194A2C'];
+
+  return (
+    <ResponsiveContainer height={400} >
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          cx={200}
+          cy={200}
+          labelLine={false}
+          outerRadius={80}
+          fill="#0088FE"
+          dataKey="value"
+          label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} name={entry.name} />))}
+        </Pie>
+        <Legend verticalAlign='bottom' height={20} />
+      </PieChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default EventGenre;
